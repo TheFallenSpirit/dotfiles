cc0d60089bbbc3acc3091414b55b2f8412572327
Liam Labell <thefallenspirit@outlook.com>
2024-07-09 02:04:35 -0700

    Update powermenu and add MPRIS controls menu

--- rofi/power/power.sh
+++ rofi/power/power.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-## Author : Aditya Shakya (adi1090x)
-## Github : @adi1090x
-
-## Modified by Liam Labell (thefallenspirit@outlook.com)
+## Made by Liam Labell (thefallenspirit@outlook.com)
 
 # Options
 shutdown=''
@@ -11,13 +8,21 @@ reboot=''
 lock=''
 suspend=''
 logout=''
+firmware=''
 
-# Check launch arguments for display mode, fullscreen or dropdown
-rofi_options="-theme power/dropdown.rasi -location 1 -xoffset $(( $(hyprctl cursorpos -j | jq .x) - 30 )) -yoffset 33"
-if [ "$1" == "--fullscreen" ]; then rofi_options="-theme power/fullscreen.rasi -p \"Goodbye ${USER}\" -mesg \"Uptime: $(uptime -p | sed -e 's/up //g')\""; fi
+# Calculate offset for popup power menu
+x_location=$(( $(wlr-randr | grep 'current' | grep -oP '\b\d+(?=x)') - 390))
+
+# Fetch the system uptime and set the menu icon
+uptime=$(uptime -p | sed -e 's/up //g')
+menu_icon="-theme-str 'textbox-prompt-colon { str: \"\"; }'"
+
+# Check the launch arguments for fullscreen to determine mode
+rofi_options="-theme controls.rasi -location 1 -xoffset $x_location -yoffset 33 -p \"Uptime: $uptime\""
+if [ "$1" == "--fullscreen" ]; then rofi_options="-theme power/fullscreen.rasi -p \"Goodbye ${USER}\" -mesg \"Uptime: $uptime\""; fi
 
 # Run Rofi power menu in fullscreen or dropdown mode
-chosen="$(echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | eval rofi -dmenu $rofi_options)"
+chosen="$(echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown\n$firmware" | eval rofi -dmenu $rofi_options $menu_icon)"
 
 # Execute based on chosen value
 case ${chosen} in
